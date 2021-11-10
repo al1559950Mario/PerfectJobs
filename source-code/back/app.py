aa6be6c25af6eee9ceb2961483d077fe9e3e9608
CeciliaMendoza01 <cecilia.mendoza@train.ia.center>
2021-11-09 20:25:17 -0700

    inicio_sesion

--- source-code/back/app.py
+++ source-code/back/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from MySQLdb import cursors
+from flask import Flask, render_template, request,redirect, session, flash
 from flask_mysqldb import MySQL
 
 #Se crea una aplicacion flask
@@ -12,14 +13,71 @@ app.config['MYSQL_DB']='bfrbtunhryqfh3eywkgx'
 
 mysql=MySQL(app)
 
+app.secret_key = b'w\xbdE[p+\xf5\xf8-\xc9\xbc~\x9f\xd1y\x14'
 
 #@app.route("") se definen rutas o enlaces en la pagina web es decir interfaces.
 @app.route("/")
 def Index():
-    return render_template('index.html')
+    return render_template('login.html')
 @app.route("/login.html")
 def login():
     return render_template('login.html')
+
+@app.route("/index.html")
+def index():
+    return render_template('index.html')
+
+@app.route("/hacer_login", methods=["POST"])
+def hacer_login():
+    correo = request.form["correo"]
+    contra = request.form["contraseña"]
+    #print(correo)
+    #print(contra)
+    cursor=mysql.connection.cursor()
+
+    sql_empresas = "SELECT * FROM empresa where correoElectronico= '"+correo+"' and Contraseña='"+contra+"'"
+    sql_aspirantes= "SELECT * FROM empresa where correoElectronico= '"+correo+"' and Contraseña='"+contra+"'"
+
+    try:
+        cursor.execute(sql_aspirantes)
+        results = cursor.fetchall()
+        print(results)
+        if len(results)==1:
+            session["usuario"] = correo
+            #home de aspirante
+            return redirect("/index.html")
+        else:
+            cursor.execute(sql_empresas)
+            results = cursor.fetchall()
+            print(results)
+            if len(results)==1:
+                session["usuario"] = correo
+                #home de empresa
+                return redirect("/index.html")
+            else:
+                # Si NO coincide, lo regresamos
+                flash("Correo o contraseña incorrectos")
+                return redirect("/login.html")
+    except:
+        return redirect("/login.html")
+
+
+# Un "middleware" que se ejecuta antes de responder a cualquier ruta. Aquí verificamos si el usuario ha iniciado sesión
+@app.before_request
+def antes_de_cada_peticion():
+    ruta = request.path
+    # Si no ha iniciado sesión y no quiere ir a algo relacionado al login, lo redireccionamos al login
+    if not 'usuario' in session and ruta != "/login.html" and ruta != "/hacer_login" and ruta != "/logout" and not ruta.startswith("/static"):
+        flash("Inicia sesión para continuar")
+        return redirect("/login.html")
+    # Si ya ha iniciado, no hacemos nada, es decir lo dejamos pasar
+
+# Cerrar sesión
+@app.route("/logout")
+def logout():
+    session.pop("usuario", None)
+    return redirect("/login")
+
 @app.route("/prueba_db")
 def prueba_db():
     cur=mysql.connection.cursor()
