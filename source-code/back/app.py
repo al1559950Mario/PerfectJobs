5c6cb74c3157033d497f9b2aed917cb78f3dbf5a
CeciliaMendoza01 <cecilia.mendoza@train.ia.center>
2021-11-12 19:24:07 -0700

    mostrar empleos

--- source-code/back/app.py
+++ source-code/back/app.py
@@ -63,8 +63,19 @@ def registroUsuario():
     return render_template('registroUsuarios.html')
 
 @app.route("/homepage")
-def index():
-    return render_template('homepage.html')
+@app.route("/homepage/<int:id>")
+def index(id = 1):
+    #data usu
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM aspirantes WHERE id ={0}".format(session["usuario"]))
+    data_usu = cur.fetchall()    
+    #data empleos
+    sql = "SELECT job.Titulo, job.Ubicacion,c.Nombre, c.fotoPerfil  FROM empleos job, empresa c WHERE job.idEmpresa = c.ID LIMIT {0}, 10".format( str(10*(id-1)))
+    print(sql)
+    cur.execute(sql)
+    data = cur.fetchall()
+
+    return render_template('homepage.html', info_usu = data_usu[0], empleos = data )
 
 @app.route("/hacer_login", methods=["POST"])
 def hacer_login():
@@ -82,7 +93,7 @@ def hacer_login():
         results = cursor.fetchall()
         print(results)
         if len(results)==1:
-            session["usuario"] = correo
+            session["usuario"] = results[0][0]
             #home de aspirante
             return redirect("/homepage")
         else:
@@ -90,7 +101,7 @@ def hacer_login():
             results = cursor.fetchall()
             print(results)
             if len(results)==1:
-                session["usuario"] = correo
+                session["usuario"] = results[0][0]
                 #home de empresa
                 return redirect("/homepage")
             else:
